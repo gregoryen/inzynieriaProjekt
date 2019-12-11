2b43049ce04b33db6dcf112a848ae8fca0a848f6
jkkostrzewski <45603875+jkkostrzewski@users.noreply.github.com>
2019-12-11 18:21:15 +0100

    Add routing

---
+++ front-end/src/components/ShoppingCart.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="shoppingCart">
+    <p> koszyczek </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShoppingCart',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import HelloWorld from './components/HelloWorld'
+import ShoppingCart from './components/ShoppingCart'
+
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      component: HelloWorld
+    },
+    {
+      path: '/shoppingCart',
+      component: ShoppingCart
+    }
+  ]
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
