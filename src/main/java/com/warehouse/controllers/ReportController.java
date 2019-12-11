68d8cd9cb297c48ac0fccdf415ed6e69fbb77944
mic216 <michal.wlodarczyk85@wp.pl>
2019-12-11 23:11:31 +0100

    Exporting report from database to local csv file

--- src/main/java/com/warehouse/controllers/ReportController.java
+++ src/main/java/com/warehouse/controllers/ReportController.java
@@ -8,9 +8,11 @@ import com.warehouse.repositories.ReportRepository;
 import com.warehouse.repositories.StockAmountChangeRepository;
 import com.warehouse.repositories.StockAmountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -45,20 +47,69 @@ public class ReportController {
         for (StockAmountChange stockAmountChange : stockAmountChanges) {
             stockAmountChangesList.add(stockAmountChange);
         }
-        Report report = new Report();
-        report.setCreationDateTime(LocalDateTime.now());
-        report.setStartDateTime(start);
-        report.setEndDateTime(end);
-        report.setStockAmountChanges(stockAmountChangesList);
-        reportRepository.save(report);
-        return "Created";
+        String returnStatement;
+        if (stockAmountChangesList != null) {
+            Report report = new Report();
+            report.setCreationDateTime(LocalDateTime.now());
+            report.setStartDateTime(start);
+            report.setEndDateTime(end);
+            report.setStockAmountChanges(stockAmountChangesList);
+            reportRepository.save(report);
+            returnStatement = "Report created";
+        } else {
+            // jakis wyjatek czy HTTP blad
+            returnStatement = "There are not any stock amount changes to include in report.";
+        }
+        return returnStatement;
     }
 
     @PostMapping("/export")
-    public String exportToCSVFileFromDatabase(@RequestParam("filepath") String filepath,
-                                              @RequestParam("filepath") String startDateTime,
-                                              @RequestParam("filepath") String endDateTime) {
-        // findall
+    public String exportToCSVFileFromDatabase(@RequestBody Map<String, String> filepathAndReportId) {
+        try {
+            String filepath = filepathAndReportId.get("filepath");
+            Integer reportId = Integer.parseInt(filepathAndReportId.get("reportId"));
+
+            Report report = reportRepository.findByReportId(reportId).
+                    orElseThrow(() -> new ResourceNotFoundException("Report not found with reportId"));
+            List<StockAmountChange> stockAmountChanges = report.getStockAmountChanges();
+
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filepath), StandardCharsets.ISO_8859_1));
+            String separator = ",";
+            StringBuffer stringBuffer = new StringBuffer();
+            stringBuffer.append("\"Id zmiany\",\"Poprzednia ilosc\",\"Aktualna ilosc\",\"Data zmiany\",\"Informacja o zmianie\",\"Id produktu\",\"Id kategorii\"");
+            bufferedWriter.write(stringBuffer.toString());
+            bufferedWriter.newLine();
+            for (StockAmountChange stockAmountChange : stockAmountChanges) {
+                stringBuffer = new StringBuffer();
+                stringBuffer.append("\""+stockAmountChange.getStockAmountChangeId()+"\"");
+                stringBuffer.append(separator);
+                stringBuffer.append("\""+stockAmountChange.getPreviousAmount()+"\"");
+                stringBuffer.append(separator);
+                stringBuffer.append("\""+stockAmountChange.getCurrentAmount()+"\"");
+                stringBuffer.append(separator);
+                stringBuffer.append("\""+stockAmountChange.getChangeDateTime()+"\"");
+                stringBuffer.append(separator);
+                String changeInfo = stockAmountChange.getChangeInfo();
+                if (changeInfo.equals("SOLD_OUT")) {
+                    changeInfo = "wyprzedano";
+                } else if (changeInfo.equals("PROVIDED")) {
+                    changeInfo = "dostarczono";
+                } else if (changeInfo.equals("SOLD")) {
+                    changeInfo = "sprzedano";
+                }
+                stringBuffer.append("\""+changeInfo+"\"");
+                stringBuffer.append(separator);
+                stringBuffer.append("\""+stockAmountChange.getStockAmount().getProductId()+"\"");
+                stringBuffer.append(separator);
+                stringBuffer.append("\""+stockAmountChange.getStockAmount().getCategoryId()+"\"");
+                bufferedWriter.write(stringBuffer.toString());
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
         return "Exported";
     }
 
--- src/main/java/com/warehouse/repositories/ReportRepository.java
+++ src/main/java/com/warehouse/repositories/ReportRepository.java
@@ -4,7 +4,10 @@ import com.warehouse.models.Report;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ReportRepository extends CrudRepository<Report, Integer> {
 
+    Optional<Report> findByReportId(Integer reportId);
 }
