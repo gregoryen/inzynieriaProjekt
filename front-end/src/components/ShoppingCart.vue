eb157da193d3c189c4bd77de372de05ce4b9097b
jkkostrzewski <45603875+jkkostrzewski@users.noreply.github.com>
2019-12-12 21:32:59 +0100

    Order summary page in progress

--- front-end/src/components/ShoppingCart.vue
+++ front-end/src/components/ShoppingCart.vue
@@ -12,16 +12,17 @@
               <ul class="cartItems" v-for="item in cartItems" v-bind:key="item.itemName">
                 <b-row>
                   <b-col sm="4">
-                    <img src="../assets/logo.png" height="85px" width="85px" />
+                    <img src="../assets/kremowka.jpeg" height="85px" width="85px" />
                   </b-col>
                   <b-col sm="4">
                     <li>{{item.itemName}}</li>
                     <li v-if="item.isAvailable === 'true'">Dostepny</li>
                     <li v-else>Niedostepny</li>
-                    <li>{{item.price}}</li>
+                    <li>{{item.price}} europapierze</li>
                   </b-col>
                   <b-col sm="2">
-                    <input type="number" min="1" max="10" step="1" value="1" size="2" />
+                    <input class="itemNumberSpinner" v-if="item.isAvailable === 'true'" type="number" min="1" max="10" step="1" value="1" size="2" />
+                    <input class="itemNumberSpinner" v-else type="number" min="0" max="0" step="1" value="0" size="2" />
                   </b-col>
                   <b-col>
                       <b-button squared class="deleteItemButton">Kosz</b-button>
@@ -32,7 +33,10 @@
           </b-col>
           <b-col cols="4">
             <div class="cartSummary">
-              <p>{{cartItems.length}} produktow</p>
+              <p v-if="cartItems.length > 1">{{cartItems.length}} produktow</p>
+              <p v-else>{{cartItems.length}} produkt</p>
+              <p>Wysyłka: Chcesz wysyłke za darmo? Pierdol sie 32 zł</p>
+              <p>Razem: {{this.calcCartPrice()}} europapierze</p>
               <router-link to="/orderSummary" tag="b-button">Przejdz do kasy</router-link>
             </div>
           </b-col>
@@ -40,8 +44,6 @@
       </div>
 
       <div style="float: left;">
-        <router-link to="/orderSummary" tag="b-button">Przejdz do kasy</router-link>
-        <br/>
         <router-link to="/" tag="b-button">Kontynuuj zakupy</router-link> <!-- Zmienic na poprzednia strone z route'a -->
       </div>
     </div>
@@ -57,20 +59,51 @@ export default {
       cartItems: [
         {
           itemName: "Zmywak do cipy",
-          price: "25 zł",
+          price: 25,
           isAvailable: "true",
           img: "../assets/logo.png",
           quantity: 1
         },
         {
           itemName: "Poduszka pod pizde",
-          price: "2137 papieży",
+          price: 2137,
+          isAvailable: "false",
+          img: "../assets/logo.png",
+          quantity: 1
+        },
+        {
+          itemName: "Wiosna dla biedronia",
+          price: 7,
+          isAvailable: "true",
+          img: "../assets/logo.png",
+          quantity: 1
+        },
+        {
+          itemName: "Kremowka tak o",
+          price: 2005,
+          isAvailable: "true",
+          img: "../assets/logo.png",
+          quantity: 1
+        },
+        {
+          itemName: "Upojna noc z Guzkiem",
+          price: 69,
           isAvailable: "false",
           img: "../assets/logo.png",
           quantity: 1
         }
       ]
     };
+  },
+  methods: {    //prawdopodobnie sie zmieni po zebraniu danych z bazy
+      calcCartPrice: function() {
+        var totalPrice = 0;
+        for(let item of this.cartItems)
+        {
+            totalPrice = totalPrice + item.price;
+        }
+        return totalPrice;
+      }
   }
 };
 </script>
@@ -96,6 +129,10 @@ ul {
 }
 
 .deleteItemButton {
+    margin-top: 20px;
+}
+
+.itemNumberSpinner {
     margin-top: 20px;
 }
 
