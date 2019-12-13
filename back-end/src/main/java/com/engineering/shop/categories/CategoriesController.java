e746f4eaa675a98f12fba7865a0f8d04a4414e79
bomblozaurus <49494671+bomblozaurus@users.noreply.github.com>
2019-12-13 20:11:22 +0100

    Dodałem endpoint, który zwraca kategorie w formie drzewa - żeby nie robić tego na froncie Dodałem Apache Commons i Collection Utils

--- back-end/src/main/java/com/engineering/shop/categories/CategoriesController.java
+++ back-end/src/main/java/com/engineering/shop/categories/CategoriesController.java
@@ -1,12 +1,15 @@
 package com.engineering.shop.categories;
 
+import lombok.Data;
+import org.apache.commons.collections4.IteratorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/categories")
@@ -21,5 +24,46 @@ public class CategoriesController {
     @GetMapping("/children")
     public Optional<Category> getChildren(@RequestParam Integer parentId) {
         return categoriesRepo.findByParentId(parentId);
+    }
+
+    @GetMapping("/tree")
+    public Iterable<TreeNode> getCategoriesTree() {
+        Iterable<Category> allCategories = categoriesRepo.findAll();
+        List<Category> categories = IteratorUtils.toList(allCategories.iterator());
+        Collection<Category> rootCategories = categories.stream().filter(category -> category.getParentId() == null).collect(Collectors.toSet());
+        categories.removeAll(rootCategories);
+        List<TreeNode> trees = rootCategories.stream().map(TreeNode::new).collect(Collectors.toList());
+        trees.forEach(treeNode -> buildTree(treeNode, categories));
+
+        return trees;
+    }
+
+    private void buildTree(TreeNode root, List<Category> categories) {
+        List<Category> children = categories.stream().filter(category -> category.getParentId().equals(root.getCategory().getId())).collect(Collectors.toList());
+        categories.removeAll(children);
+
+        Category topChild = new Category();
+        List<Category> sortedChildren = new ArrayList<>();
+        while (children.size() > 0) {
+            Integer topChildId = topChild.getId();
+            topChild = children.stream().filter(child -> Objects.equals(topChildId, child.getPreviousCategoryId())).findFirst().orElseThrow(IllegalArgumentException::new);
+            sortedChildren.add(topChild);
+            children.remove(topChild);
+        }
+
+        List<TreeNode> childNodes = sortedChildren.stream().map(TreeNode::new).collect(Collectors.toList());
+        root.setChildren(childNodes);
+        childNodes.forEach(node -> this.buildTree(node, categories));
+    }
+
+    @Data
+    private class TreeNode {
+        Category category;
+        Iterable<TreeNode> children;
+
+        TreeNode(Category category) {
+            this.category = category;
+            this.children = new ArrayList<>();
+        }
     }
 }
