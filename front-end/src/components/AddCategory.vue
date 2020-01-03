a003a524aeedb0cad3a4806f6b17c60bc6ba96f1
rjehf <45949354+rjehf@users.noreply.github.com>
2020-01-03 14:23:45 +0100

    Add possibility to add new main category

--- front-end/src/components/AddCategory.vue
+++ front-end/src/components/AddCategory.vue
@@ -28,21 +28,31 @@
             </b-form-group>
 
             <b-form-group
+                    id="addMainCategory"
+                    label="Czy chcesz dodać kategorię główną:"
+                    label-for="ean13"
+            >
+                <b-form-checkbox v-model="supported.mainCategory" name="check-button" @change="addMainCategory" switch>
+                </b-form-checkbox>
+                <b v-if="supported.mainCategory">Tak</b>
+                <b v-else>Nie</b>
+            </b-form-group>
+
+            <b-form-group
                     id="mainCategory"
                     label="Wybierz kategorię nadrzędną: "
-                    label-for="searcher">
-                <b-form-input
-                        id="searcher"
-                        v-model="supported.searchMainCategories"
-                        type="text"
-                        placeholder="Wpisz szukaną kategorię"
-                ></b-form-input>
-                <div>
-                    <b-form-select v-model="form.parentId"
-                                   :options="filteredMainCategoriesId" :select-size="4"
-                                   required
-                                   @change="getChildren"></b-form-select>
-                </div>
+                    label-for="searcher"
+                    v-if="!supported.mainCategory">
+
+                <treeselect
+                        :options="this.supported.treeCategories"
+                        v-model="form.parentId"
+                        :searchable="false"
+                        :show-count="true"
+                        @input="getChildren"
+                        :default-expand-level="1"
+                >
+                </treeselect>
             </b-form-group>
 
             <b-form-group
@@ -59,16 +69,6 @@
                                    :variant="category.variant">{{category.text}}
                 </b-list-group-item>
             </b-list-group>
-
-            <treeselect
-                    :options="this.supported.treeCategories"
-                    v-model="form.parentId"
-                    :searchable="false"
-                    :show-count="true"
-                    @input="getChildren"
-                    :default-expand-level="1"
-            >
-            </treeselect>
 
             <b-button type="submit" variant="primary">Submit</b-button>
             <b-button type="reset" variant="danger">Reset</b-button>
@@ -89,6 +89,7 @@
     const CATEGORIES_WITH_ID = '/categories?projection=withId';
     const CATEGORIES_CHILDREN = '/categories/children';
     const CATEGORIES_TREE = '/categories/tree';
+    const CATEGORIES_MAIN = '/categories/search/findAllByParentIdIsNull?projection=withId';
 
     export default {
         // register the component
@@ -110,7 +111,8 @@
                     allCategories: [],
                     childrenCategories: [],
                     selectedPosition: 1,
-                    positions: []
+                    positions: [],
+                    mainCategory: false
                 },
                 newCategory: {
                     value: 0,
@@ -127,12 +129,12 @@
             onReset(evt) {
                 evt.preventDefault()
                 // Reset our form values
-                this.form.name = ''
-                this.form.description = ''
-                this.form.parentId = null
-                this.form.previousCategoryId = null
+                this.form.name = '';
+                this.form.description = '';
+                this.form.parentId = null;
+                this.form.previousCategoryId = null;
                 // Trick to reset/clear native browser form validation state
-                this.show = false
+                this.show = false;
                 this.$nextTick(() => {
                     this.show = true
                 })
@@ -145,6 +147,11 @@
                     this.supported.childrenCategories.splice(oldValue - 1, 1);
                 }
                 this.supported.childrenCategories.splice(newValue - 1, 0, this.newCategory);
+                if (newValue === 1) {
+                    this.form.previousCategoryId = null;
+                } else {
+                    this.form.previousCategoryId =  this.supported.childrenCategories[newValue-2].value;
+                }
             },
             getChildren() {
                 this.supported.childrenCategories = [];
@@ -175,6 +182,44 @@
                             }
                         }
                     );
+            },
+            getMainCategories() {
+                this.supported.childrenCategories = [];
+                this.supported.positions = [];
+                const config = {
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                };
+                axios.get(this.baseurl + CATEGORIES_MAIN, config)
+                    .then(res => {
+                            if (res.status === 200) {
+                                for (let i = 0; i < res.data._embedded.categories.length; i++) {
+                                    let item = {
+                                        value: res.data._embedded.categories[i].id,
+                                        text: res.data._embedded.categories[i].name,
+                                        variant: null
+                                    };
+                                    this.supported.positions.push(i + 1);
+                                    this.supported.childrenCategories.push(item);
+                                }
+                                this.supported.positions.push(this.supported.positions.length + 1);
+                                this.supported.childrenCategories.splice(this.supported.selectedPosition - 1, 0, this.newCategory);
+                                this.supported.selectedPosition = 1;
+                            }
+                        }
+                    );
+            },
+            addMainCategory(value) {
+                if (value) {
+                    this.form.parentId = null;
+                    this.supported.mainCategory = true;
+                    this.getMainCategories();
+                } else {
+                    this.supported.mainCategory = false;
+                    this.supported.childrenCategories = [];
+                    this.supported.positions = [];
+                }
             },
             createBranch: function (oldBranch) {
                 let newBranch = [];
