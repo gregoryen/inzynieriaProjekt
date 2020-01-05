a14241b1508b1fade881cd49768bd82c9a7230e7
rjehf <45949354+rjehf@users.noreply.github.com>
2020-01-05 16:27:43 +0100

    Change productHeader

--- front-end/src/components/ProductHeader.vue
+++ front-end/src/components/ProductHeader.vue
@@ -1,35 +1,21 @@
 <template>
-    <li>
-            <b-container>
-                <b-row align-h="center" class="mt-5">
-                    <b-col cols="10">
-                        <div>
-                            <b-card style="background-color: grey">
-                                <b-card-body>
-                                    <b-container class="bv-example-row bv-example-row-flex-cols">
-                                        <b-row>
-                                            <b-col cols="4" align-self="start">
-                                                <img :src="this.productHeader._links.mainImage.href" width="300"
-                                                     height="220">
-                                            </b-col>
-                                            <b-col cols="1"/>
-                                            <b-col cols="5" class="nameFont" align-self="center">{{productHeader.name}}</b-col>
-                                            <b-col cols="2" class="nameFont" align-self="end">
-                                                <router-link :to="{name: 'product', params: {link: productHeader._links.self.href, baseurl: this.baseurl}}">
-                                                    <b-button variant="success">Zobacz szczegóły</b-button>
-                                                </router-link>
-                                                Cena:
-                                                <br/> {{productHeader.price}} PLN
-                                            </b-col>
-                                        </b-row>
-                                    </b-container>
-                                </b-card-body>
-                            </b-card>
-                        </div>
-                    </b-col>
-                </b-row>
-            </b-container>
-    </li>
+    <div class="card product-item">
+        <img :src="this.productHeader._links.mainImage.href" width="250px" height="250px"
+             class="card-img-top" alt="Zdjęcie produktu">
+
+        <div class="card-body">
+            <div class="d-flex justify-content-between">
+                <h5 class="card-title">{{productHeader.name}}</h5>
+                <h6 class="card-price">{{productHeader.price}} PLN</h6>
+
+            </div>
+
+            <router-link :to="{name: 'product', params: {link: productHeader._links.self.href, baseurl: this.baseurl}}">
+                <b-button variant="success">Zobacz szczegóły</b-button>
+            </router-link>
+        </div>
+
+    </div>
 </template>
 
 <script>
@@ -43,22 +29,7 @@
     };
 </script>
 <style scoped>
-    @import url('https://fonts.googleapis.com/css?family=Lato:400,700');
-    #product {
-        display: flex;
-
-    }
-
-    li > a {
-        height: 60px;
-    }
-
-    div:first-child {
-        padding-right: 20px;
-    }
-
-    .nameFont {
-        font-family: 'Lato', sans-serif;
-        font-size: 20px;
+    .card {
+        margin-bottom: 1rem;
     }
 </style>
--- front-end/src/components/Products.vue
+++ front-end/src/components/Products.vue
@@ -1,69 +1,78 @@
 <template>
-  <div id="productContainer">
-    <p id="emptyProductsMessage" v-if="productsHeader === null || productsHeader.length === 0">BRAK PRODUKTÓW</p>
-    <ul v-else>
-      <ProductHeader v-for="productHeader in productsHeader" v-bind:key="productHeader.name" :productHeader="productHeader" :baseurl="baseurl" />
-    </ul>
-  </div>
+    <div id="productContainer">
+        <p id="emptyProductsMessage" v-if="productsHeader === null || productsHeader.length === 0">BRAK PRODUKTÓW</p>
+        <div v-else class="products" id="products">
+            <div class="container">
+                <h1 class="text-center p-5">Nasze Produkty</h1>
+                <div class="row">
+                    <div class="col-md-4" v-for="productHeader in productsHeader" v-bind:key="productHeader.name">
+                        <ProductHeader :productHeader="productHeader" :baseurl="baseurl"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
-import axios from "axios";
-import { bus } from "../main.js";
-import ProductHeader from "./ProductHeader.vue";
+    import axios from "axios";
+    import {bus} from "../main.js";
+    import ProductHeader from "./ProductHeader.vue";
 
-const UPLOAD_ALL_ACTIVE_HEADER_PRODUCTS = "/products/search/findAllByActiveIsTrue?projection=header";
+    const UPLOAD_ALL_ACTIVE_HEADER_PRODUCTS = "/products/search/findAllByActiveIsTrue?projection=header";
 
-export default {
-  components: { ProductHeader },
-  name: "Products",
-  props: {
-    baseurl: String
-  },
-  data: () => {
-    return {
-      productsHeader: null
+    export default {
+        components: {ProductHeader},
+        name: "Products",
+        props: {
+            baseurl: String
+        },
+        data: () => {
+            return {
+                productsHeader: null
+            };
+        },
+        mounted() {
+            bus.$on("products", products => {
+                axios
+                    .get(products)
+                    .then(response => {
+                        this.productsHeader = response.data._embedded.products;
+                    });
+                this.productsHeader = products;
+            });
+        },
+        created() {
+            if (this.productsHeader === null) {
+                axios
+                    .get(
+                        this.baseurl + UPLOAD_ALL_ACTIVE_HEADER_PRODUCTS
+                    )
+                    .then(response => {
+                        this.productsHeader = response.data._embedded.products;
+                    });
+            }
+        },
+        methods: {
+            getProducts(event) {
+                this.productsHeader = event;
+            }
+        }
     };
-  },
-  mounted() {
-    bus.$on("products", products => {
-      axios
-              .get(products)
-              .then(response => {
-                this.productsHeader = response.data._embedded.products;
-              });
-      this.productsHeader = products;
-    });
-  },
-  created() {
-      if (this.productsHeader === null) {
-        axios
-          .get(
-            this.baseurl + UPLOAD_ALL_ACTIVE_HEADER_PRODUCTS
-          )
-          .then(response => {
-            this.productsHeader = response.data._embedded.products;
-          });
-      }
-  },
-  methods: {
-    getProducts(event) {
-      this.productsHeader = event;
-    }
-  }
-};
 </script>
 
 <style scoped>
-#productContainer {
-  margin-top: 15px;
-}
+    #productContainer {
+        margin-top: 15px;
+    }
 
-#emptyProductsMessage {
-  text-align: center;
-  font-size: 30;
-}
-ul {
-  list-style-type: none;
-}
+    #emptyProductsMessage {
+        text-align: center;
+        font-size: 30;
+    }
+
+    ul {
+        list-style-type: none;
+    }
 </style>
