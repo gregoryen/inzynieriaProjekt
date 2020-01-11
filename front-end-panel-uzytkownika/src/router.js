b6e2adac342e40fcc49be8b4cc09fb6477f3830f
Mariusz <mariusz.jozwiak@int.pl>
2020-01-12 00:47:24 +0100

    Dodanie ścieżki do strony modyfikacji

--- front-end-panel-uzytkownika/src/router.js
+++ front-end-panel-uzytkownika/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Login from './views/Login.vue';
 import Register from './views/Register.vue';
+import Modify from './views/Modify.vue';
 
 Vue.use(Router);
 
@@ -25,6 +26,10 @@ export const router = new Router({
     {
       path: '/register',
       component: Register
+    },
+    {
+      path: '/modify',
+      component: Modify
     },
     {
       path: '/profile',
