3ca56cfec2c9573876e89f0506f61e0cdbaee14c
jkkostrzewski <45603875+jkkostrzewski@users.noreply.github.com>
2020-01-12 16:15:02 +0100

    main.js fix

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -6,12 +6,19 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VueCurrencyInput from 'vue-currency-input'
-
 import ShoppingCart from './components/ShoppingCart'
 import OrderSummary from './components/OrderSummary'
 import AddProduct from "./components/AddProduct";
 import Home from './components/Home'
 import AdminPanel from './components/AdminPanel'
+
+import Categories from './components/Categories.vue'
+import Products from './components/Products.vue'
+import Product from './components/Product.vue'
+import AddCategory from "./components/AddCategory";
+import DeleteCategory from "./components/DeleteCategory";
+import DeleteProduct from "./components/DeleteProduct";
+import {store} from './store';
 
 const pluginOptions = {
   globalOptions: { currency: 'PLN' }
@@ -21,38 +28,10 @@ Vue.use(VueRouter);
 Vue.use(BootstrapVue);
 Vue.use(VueCurrencyInput, pluginOptions)
 
-
-import Categories from './components/Categories.vue'
-import Products from './components/Products.vue'
-import Product from './components/Product.vue'
-import AddCategory from "./components/AddCategory";
-import AddProduct from "./components/AddProduct";
-import DeleteCategory from "./components/DeleteCategory";
-import DeleteProduct from "./components/DeleteProduct";
-import {store} from './store';
-
-Vue.use(VueRouter)
-Vue.use(BootstrapVue)
-Vue.use(VueCurrencyInput, pluginOptions)
-
 Vue.config.productionTip = false;
 
-const routes = [
-  { path: '/product', name: 'product', component: Product },
-  { path: '/', name: 'products', component: Products },
-  { path: '/categories', name: 'categories', component: Categories },
-  { path: '/addCategory', name: 'addCategory', component: AddCategory},
-  { path: '/addProduct', name: 'addProduct', component: AddProduct },
-  { path: '/deleteCategory', name: 'deleteCategory', component: DeleteCategory },
-  { path: '/deleteProduct', name: 'deleteProduct', component: DeleteProduct },
-];
-
-const pluginOptions = { globalOptions: { currency: 'PLN' } };
-
-const router = new VueRouter({ routes });
 
 export const bus = new Vue();
-
 
 
 const router = new VueRouter({
@@ -76,7 +55,14 @@ const router = new VueRouter({
     {
       path: '/adminPanel',
       component: AdminPanel
-    }
+    },
+    { path: '/product', name: 'product', component: Product },
+    { path: '/products', name: 'products', component: Products },
+    { path: '/categories', name: 'categories', component: Categories },
+    { path: '/addCategory', name: 'addCategory', component: AddCategory},
+    { path: '/addProduct', name: 'addProduct', component: AddProduct },
+    { path: '/deleteCategory', name: 'deleteCategory', component: DeleteCategory },
+    { path: '/deleteProduct', name: 'deleteProduct', component: DeleteProduct }
   ]
 })
 
