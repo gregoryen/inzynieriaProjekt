61c318c3e1db02ebe07788c1e963b8e65da6da2c
Mariusz <mariusz.jozwiak@int.pl>
2020-01-13 00:08:36 +0100

    Dodane dwie metody do resetu

--- back-end/src/main/java/com/engineering/shop/security/IUserService.java
+++ back-end/src/main/java/com/engineering/shop/security/IUserService.java
@@ -5,5 +5,7 @@ import com.engineering.shop.users.UserSignUpPOJO;
 
 public interface IUserService {
     User registerNewUserAccount(UserSignUpPOJO account);
+    void createPasswordResetTokenForUser(User user, String token);
+    String validatePasswordResetToken(long id, String token);
     //User changeUserData(UserSignUpPOJO account);
 }
