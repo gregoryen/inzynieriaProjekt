f736877ec1c239fb18cba470d7bc91f656bf58a5
AdamGniadzik <56366799+AdamGniadzik@users.noreply.github.com>
2020-01-13 00:13:35 +0100

    A zatem nastała światłość

--- front-end/src/components/AdminPanel.vue
+++ front-end/src/components/AdminPanel.vue
@@ -1,16 +1,5 @@
 <template>
   <div>
-    <b-navbar toggleable="sm" type="dark" variant="dark">
-      <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
-
-      <b-navbar-brand>Garden Shop</b-navbar-brand>
-
-      <b-collapse id="nav-text-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-text>Admin Panel</b-nav-text>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
     <b-row>
       <b-col cols="6" md="4">
         <div class="small-table">
@@ -26,6 +15,9 @@
               </tr>
               <tr>
                 <td @click="getRolesResponseData">Show Roles</td>
+              </tr>
+              <tr>
+                <td @click="getPrivilegesResponseData">Show Privileges</td>
               </tr>
             </tbody>
           </table>
@@ -43,6 +35,7 @@
                 <th>First Name</th>
                 <th>Last Name</th>
                 <th>Edit</th>
+                <th>Delete</th>
               </tr>
             </thead>
             <tbody>
@@ -51,7 +44,10 @@
                 <td>{{ item.firstName }}</td>
                 <td>{{ item.lastName }}</td>
                 <td>
-                  <b-button pill variant="success" size="sm">Edit</b-button>
+                  <b-button pill variant="warning" size="sm">Edit</b-button>
+                </td>
+                <td>
+                  <b-button pill variant="danger" size="sm">Delete</b-button>
                 </td>
               </tr>
             </tbody>
@@ -64,14 +60,33 @@
               <tr>
                 <th>Role</th>
                 <th>Edit</th>
+                <th>Delete</th>
               </tr>
             </thead>
             <tbody>
-              <tr v-for="item in mainTable" v-bind:key="item.email">
+              <tr v-for="item in mainTable" v-bind:key="item.name">
                 <td>{{ item.name }}</td>
                 <td>
-                  <b-button pill variant="success" size="sm">Edit</b-button>
+                  <b-button pill variant="warning" size="sm">Edit</b-button>
                 </td>
+                <td>
+                  <b-button pill variant="danger" size="sm">Delete</b-button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <table
+            class="table b-table table-sm table-bordered"
+            v-if="this.changeMainTable === 'privileges'"
+          >
+            <thead class="thead-light">
+              <tr>
+                <th>Privileges</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in mainTable" v-bind:key="item.name">
+                <td>{{ item.name }}</td>
               </tr>
             </tbody>
           </table>
@@ -83,7 +98,6 @@
 
 <script>
 import axios from "axios";
-
 export default {
   name: "AdminPanel",
   data() {
@@ -91,17 +105,6 @@ export default {
       changeMainTable: "empty",
       items: [{ option: "Show users" }, { option: "Show roles" }],
       mainTable: [],
-      mainTableHeaders: ["email", "firstName", "lastName"],
-      users: [
-        { email: "1234@wp.pl", firstName: "Alicja", lastName: "Pokrzywnicka" },
-        { email: "1212@wp.pl", firstName: "Antoni", lastName: "Ulicki" },
-        { email: "1313@wp.pl", firstName: "Kamil", lastName: "Bednarz" },
-        { email: "1414@wp.pl", firstName: "Mateusz", lastName: "Kiszonka" }
-      ],
-      roles: [
-        { name: "USER", desc: "Read permission" },
-        { name: "ADMIN", desc: "Full power" }
-      ]
     };
   },
   methods: {
@@ -119,9 +122,8 @@ export default {
     },
     getUsersResponseData: function() {
       var root = "http://localhost:8080";
-      var token =
-        "eyJhbGciOiJSUzUxMiJ9.eyJyb2xlIjoiUkVBRF9QUklWSUxFR0UiLCJuYW1lIjoiQWRhbSBHbmlhZHppayIsInN1YiI6InVzZXJAdnAucGwiLCJpYXQiOjE1NzY0MjkwNTEsImV4cCI6MTU3OTAyMTA1MX0.py1Xhjs_-mYdSCdH3nYxYJ5jl-4OFhJEm1lAVGEBSH8jwZ7haERgA1Vb2oUvh2r4OtKy3cw_T7uusEmFqxVZJhe9lLeMp1YWDgvuXSbdxw9vLh7sZCTM_P4iDGEqoRQd0M76HPtQln8v7zAKTDXGRYc3WAnT2VzabvNiKEL1i3rWekRJZ2x1htMAgjPTVVDgn9fYbfyyrfzy_hC8sQnFrCMk-nsmJU6QWFX2b69IX7B26GsJSf7xN48hJfT2hp8kYqbOWAbSfuLUQAkX6jb8HJyggyqWw4gX-m6vGPBhalCVpVsp6X7hrcna7HFUR1U81W1Dv-4BJ_U9RARLvwokPg";
-      const config = {
+      var token = this.$store.state.auth.user.jwtToken
+       const config = {
         headers: {
           Authorization: "Bearer " + token,
           "Content-type": "application/json"
@@ -142,9 +144,8 @@ export default {
     },
     getRolesResponseData: function() {
       var root = "http://localhost:8080";
-      var token =
-        "eyJhbGciOiJSUzUxMiJ9.eyJyb2xlIjoiUkVBRF9QUklWSUxFR0UiLCJuYW1lIjoiQWRhbSBHbmlhZHppayIsInN1YiI6InVzZXJAdnAucGwiLCJpYXQiOjE1NzY0MjkwNTEsImV4cCI6MTU3OTAyMTA1MX0.py1Xhjs_-mYdSCdH3nYxYJ5jl-4OFhJEm1lAVGEBSH8jwZ7haERgA1Vb2oUvh2r4OtKy3cw_T7uusEmFqxVZJhe9lLeMp1YWDgvuXSbdxw9vLh7sZCTM_P4iDGEqoRQd0M76HPtQln8v7zAKTDXGRYc3WAnT2VzabvNiKEL1i3rWekRJZ2x1htMAgjPTVVDgn9fYbfyyrfzy_hC8sQnFrCMk-nsmJU6QWFX2b69IX7B26GsJSf7xN48hJfT2hp8kYqbOWAbSfuLUQAkX6jb8HJyggyqWw4gX-m6vGPBhalCVpVsp6X7hrcna7HFUR1U81W1Dv-4BJ_U9RARLvwokPg";
-      const config = {
+      var token = this.$store.state.auth.user.jwtToken
+           const config = {
         headers: {
           Authorization: "Bearer " + token,
           "Content-type": "application/json"
@@ -157,6 +158,28 @@ export default {
           console.log(response.data._embedded.roles);
           this.mainTable = response.data._embedded.roles;
           this.changeMainTable = "roles";
+        })
+        .catch(error => {
+          /* eslint-disable no-console */
+          console.log(error);
+        });
+    },
+        getPrivilegesResponseData: function() {
+      var root = "http://localhost:8080";
+      var token = this.$store.state.auth.user.jwtToken
+            const config = {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-type": "application/json"
+        }
+      };
+      axios
+        .get(root + "/privileges", config)
+        .then(response => {
+          /* eslint-disable no-console */
+          console.log(response.data._embedded.privileges);
+          this.mainTable = response.data._embedded.privileges;
+          this.changeMainTable = "privileges";
         })
         .catch(error => {
           /* eslint-disable no-console */
