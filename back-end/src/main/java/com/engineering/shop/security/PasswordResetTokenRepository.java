002be8c484f0969eed6ad792b9888563a3114071
Mariusz <mariusz.jozwiak@int.pl>
2020-01-13 00:07:45 +0100

    Klasa przechowująca tokeny resetu

---
+++ back-end/src/main/java/com/engineering/shop/security/PasswordResetTokenRepository.java
@@ -0,0 +1,8 @@
+package com.engineering.shop.security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+	PasswordResetToken findByToken(String token);
+
+}
