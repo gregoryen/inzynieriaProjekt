794a49470b5d3c49cef2798209c8613160144313
AdamGniadzik <56366799+AdamGniadzik@users.noreply.github.com>
2020-01-13 18:12:49 +0100

    Update Login.vue

--- front-end/src/components/Login.vue
+++ front-end/src/components/Login.vue
@@ -13,7 +13,7 @@
         <div class="form-group">
           <label for="password">Password</label>
           <validation-provider rules="required" v-slot="{ errors }">
-            <input v-model="user.password" name="password" type="text" class="form-control"/>
+            <input v-model="user.password" name="password" type="password" class="form-control"/>
             <span>{{ errors[0] }}</span>
             </validation-provider>
     
