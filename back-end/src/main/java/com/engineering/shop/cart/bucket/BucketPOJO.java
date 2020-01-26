61c20cbffe7a061a5939cb4b5bc192740e707e8a
gregoryen <gregoryen@users.noreply.github.com>
2020-01-26 18:17:14 +0100

    payment-method-update

--- back-end/src/main/java/com/engineering/shop/cart/bucket/BucketPOJO.java
+++ back-end/src/main/java/com/engineering/shop/cart/bucket/BucketPOJO.java
@@ -1,10 +1,10 @@
 package com.engineering.shop.cart.bucket;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
-@Data
+import lombok.RequiredArgsConstructor;@Data
 @RequiredArgsConstructor
+
+
 public class BucketPOJO {
     private String id;
 }
